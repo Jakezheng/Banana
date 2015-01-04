2f7fefc23d52ff2b035b0432dbf84fc706b5e94d
haiqiang <haiqiangz1991@gmail.com>
2015-01-04 17:10:04 -0500

    modifed item table and removed post table

--- sql/CREATE_TABLE.sql
+++ sql/CREATE_TABLE.sql
@@ -1,5 +1,5 @@
-DROP DATABASE IF EXISTS Mango;
-CREATE DATABASE Mango;
+--DROP DATABASE IF EXISTS Mango;
+--CREATE DATABASE Mango;
 USE Mango;
 
 DROP TABLE IF EXISTS ACCOUNTS;
@@ -13,16 +13,14 @@ PRIMARY KEY (ACCOUNT_ID),
 UNIQUE (EMAIL_ADDRESS)
 ); 
 
-//ZZZ 
 DROP TABLE IF EXISTS CATEGORIES;
 CREATE TABLE CATEGORIES
 (
-CATEGORY_ID int NOT NULL AUTO_INCREMENT,
-NAME varchar(255) NOT NULL,
-IMG varchar(255) NOT NULL,
-CMT varchar(255) NOT NULL,
-PRIMARY KEY (CATEGORY_ID),
-UNIQUE (NAME)
+CATEGORY_NAME varchar(255) NOT NULL,
+CATEGORY_IMG varchar(255) NOT NULL,
+CATEGORY_CMT varchar(255) NOT NULL,
+PRIMARY KEY (CATEGORY_NAME),
+UNIQUE (CATEGORY_NAME)
 ) CHARACTER SET = utf8; 
 
 
@@ -39,19 +37,12 @@ ITEM_LOCATION varchar(255) NOT NULL,
 ITEM_PRICE varchar(255) NOT NULL,
 ITEM_STATUS varchar(255) NOT NULL,
 ITEM_PICTURES varchar(255) NOT NULL,
-ITEM_SOLD BOOLEAN NOT NULL DEFAULT 0,
-PRIMARY KEY (ITEM_ID)
-) CHARACTER SET = utf8;
-
-DROP TABLE IF EXISTS POSTS;
-CREATE TABLE POSTS
-(
-POST_ID int NOT NULL AUTO_INCREMENT,
-ITEM_ID int NOT NULL,
+ITEM_POSTDATE DATE NOT NULL,
 ACCOUNT_ID int NOT NULL,
-PRIMARY KEY (POST_ID),
-FOREIGN KEY (ITEM_ID) REFERENCES ITEMS(ITEM_ID),
-FOREIGN KEY (ACCOUNT_ID) REFERENCES ACCOUNTS(ACCOUNT_ID)
+CATEGORY_NAME varchar(255) NOT NULL,
+PRIMARY KEY (ITEM_ID),
+FOREIGN KEY (ACCOUNT_ID) REFERENCES ACCOUNTS(ACCOUNT_ID),
+FOREIGN KEY (CATEGORY_NAME) REFERENCES CATEGORIES(CATEGORY_NAME)
 ) CHARACTER SET = utf8;
 
 insert into ACCOUNTS (EMAIL_ADDRESS, PASSWORD)
@@ -60,5 +51,12 @@ values ("Singwai@mail.com" , "Password"),
  ("php@mail.com" , "Password"),
  ("Google@mail.com" , "Password");
  
--- insert into CATEGORIES (NAME,IMG)
--- values ("家具"，"http://tradeincampus.com/category_img/furniture-icon.jpg"),("电子产品","http://tradeincampus.com/category_img/electronic-icon.jpg"),("服饰","http://tradeincampus.com/category_img/clothing-icon.jpg"),("书本","http://tradeincampus.com/category_img/Book-icons.jpg"),("运动产品","http://tradeincampus.com/category_img/sports-icons.jpg"),("其他","http://tradeincampus.com/category_img/others-icon.png");
+insert into CATEGORIES (NAME,IMG)
+values ("家具"，"http://tradeincampus.com/category_img/furniture-icon.jpg"),("电子产品","http://tradeincampus.com/category_img/electronic-icon.jpg"),("服饰","http://tradeincampus.com/category_img/clothing-icon.jpg"),("书本","http://tradeincampus.com/category_img/Book-icons.jpg"),("运动产品","http://tradeincampus.com/category_img/sports-icons.jpg"),("其他","http://tradeincampus.com/category_img/others-icon.png");
+
+
+
+
+
+
+
