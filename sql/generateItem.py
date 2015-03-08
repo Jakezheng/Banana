2ffa886c9d7258a772aedb6ef5bd4f93aa171715
haiqiang <haiqiangz1991@gmail.com>
2015-03-07 20:31:54 -0500

    fixed item generation script

--- sql/generateItem.py
+++ sql/generateItem.py
@@ -18,6 +18,8 @@ print("Creating new file")
 
 text_file = open("ItemData.sql", 'w')
 
+text_file.write("USE Mango;\n")
+
 text_file.write("insert into ITEMS (ITEM_ID, ITEM_NAME, ITEM_DESCRIPTION, ITEM_CONDITION, ITEM_LOCATION, ITEM_PRICE, ITEM_STATUS, ITEM_PICTURES, ACCOUNT_ID, CATEGORY_ID)\nvalues\n")
 
 for i in range(0,30):
@@ -32,7 +34,7 @@ for i in range(0,30):
 		text_file.write("('" + str(i) + "','" + itemName[itemName_rand] + "'," + "'FUCK'" + ",'" + itemCondition[itemCond_rand] + "'," + "'FLUSHING'" + ",'" + str(randint(0,100)) +"','" + itemStatus[itemStatus_rand] + "','" + imageUrl[imageRan] + "','" + str(account_rand) + "','" + str(category) + "');")
 		break		
 
-	text_file.write("('" + str(i) + "','" + itemName[itemName_rand] + "'," + "'FUCK'" + ",'" + itemCondition[itemCond_rand] + "'," + "'FLUSHING'" + ",'" + str(randint(0,100)) +"','" + itemStatus[itemStatus_rand] + "','" + imageUrl[imageRan] + "','" + str(account_rand) + "','" + str(category) + "'),\n")
+	text_file.write("('" + itemName[itemName_rand] + "'," + "'FUCK'" + ",'" + itemCondition[itemCond_rand] + "'," + "'FLUSHING'" + ",'" + str(randint(0,100)) +"','" + itemStatus[itemStatus_rand] + "','" + imageUrl[imageRan] + "'," + str(account_rand) + "," + str(category) + "),\n")
 
 text_file.close()
 
